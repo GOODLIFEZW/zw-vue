bc131c1a5f8cb7dfad246ad1280cba998df812a7
GOODLIFEZW <zwalker1@163.com>
2019-02-05 12:12:59 +0800

    init program and add login

---
+++ src/components/Base/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="nav">
+      <img src="../../assets/logo.png" class="nav-img">
+      Design by zwalker
+      <router-lick to="/">Home</router-lick>
+      <input type="text" placeholder="Search">
+      <button>Search</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .nav
+    background #cfd3cf
+  .nav-img
+    width 30px
+    height 30px
+</style>
